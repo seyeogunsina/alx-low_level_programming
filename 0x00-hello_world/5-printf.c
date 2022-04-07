a66412bf67f8e3f72eba70c52063fa55ca0be81f
Adeseye <adeseyeogunsina@gmail.com>
2022-04-07 12:56:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#header <stdio.h>
+#header <string.h>
+/**
+ * main - a function displays a sting on the standard output with printf
+ * Return: It returns the integer 0
+ */
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf('%s', str1);
+
+	return (0);
+}
