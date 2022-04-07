7f55d2b345d6e57d312d87eca51f821c574b2f0f
Adeseye <adeseyeogunsina@gmail.com>
2022-04-07 12:34:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char str1[] = "Programming is like building a multilingual puzzle\n";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 	puts(str1);
 
 	return 0;
