dcaaacfa5d924030ef0b1b7a21f6ce1f4a06a1cc
Adeseye <adeseyeogunsina@gmail.com>
2022-04-07 12:45:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * A function that displays a string on standard output
+ */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
