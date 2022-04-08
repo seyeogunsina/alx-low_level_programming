b81f74e2e59ebcc3c115b6375b22788bcb4403fd
Adeseye <adeseyeogunsina@gmail.com>
2022-04-08 08:15:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints small aplhabets using putchar
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a', ch <= 'z', ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
