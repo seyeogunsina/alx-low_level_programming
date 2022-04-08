0c104789412a13d914637271c6af8a5ed1a791d7
Adeseye <adeseyeogunsina@gmail.com>
2022-04-08 09:16:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single character numbers
+ * Return: number 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0, i <= 9, i++)
+		putchar((char) i);
+
+	putchar('\n');
+	
+	return (0);
+}
