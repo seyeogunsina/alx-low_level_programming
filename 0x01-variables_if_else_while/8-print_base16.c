2c2e3cf88a71d63fe5651bc095e3130aadb4c70e
Adeseye <adeseyeogunsina@gmail.com>
2022-04-08 09:35:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal numbers
+ * Return: number 0
+ */
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 9, i++)
+		putchar(i + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
