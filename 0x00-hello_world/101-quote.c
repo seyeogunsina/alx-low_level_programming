832bea1a8ce49fac857f9f73a83dd8f21b19c854
Adeseye <adeseyeogunsina@gmail.com>
2022-04-09 13:30:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - a function that prints a string
+ * Return: number 1
+ */
+
+int main(void)
+{
+	char st[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	int i = 0;
+	int len = strlen(st);
+
+	for (i = 0; i < len; i++)
+	{
+		putchar(st[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
