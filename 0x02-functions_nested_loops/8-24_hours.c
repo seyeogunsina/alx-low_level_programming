e40ff294a1b148d3191ac38029643b3f16ad43ce
Adeseye <adeseyeogunsina@gmail.com>
2022-04-12 15:47:08 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minutes of the day
+ * returns: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		if (i < 10)
+		{
+			putchar('0');
+			putchar('0' + i);
+		}
+		else
+		{
+			putchar('0' + i);
+		}
+
+		for (j = 0; j < 60; j++)
+		{
+			if (j < 10)
+			{
+				putchar('0');
+				putchar('0' + j);
+			}
+			else
+			{
+				putchar('0' + j);
+			}
+		}
+	}
+}
