fbd00e9535c97492537319fe7f709591ce5bbcf2
Adeseye <adeseyeogunsina@gmail.com>
2022-04-12 14:03:30 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
+		putchar('1');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		putchar('-');
+		putchar('1');
 		return (0-1);
 	}
 	else
 	{
+		putchar('0');
 		putchar('0');
 		return (0);
 	}
