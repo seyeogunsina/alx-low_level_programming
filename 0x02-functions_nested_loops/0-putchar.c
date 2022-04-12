faa450a170d4ace4025940e6e99a179b56b1e333
Adeseye <adeseyeogunsina@gmail.com>
2022-04-12 10:40:12 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints _putchar
+ * Return: returns number 0
+ */
+
+int main(void)
+{
+	char str_o = "_putchar";
+	int i;
+
+	for (i = 0; i <= strlen(str_o)); i++)
+	{
+		putchar(str_o[i];
+	}
+
+	return (0);
+}
