d01d8e39579236041e1f3a7c183dc5e703d33cd8
Adeseye <adeseyeogunsina@gmail.com>
2022-04-12 12:33:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - 
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alphabets[27];
+	int len;
+	int i;
+
+	alphabets = [a..z];
+	len = strlen(alphabets);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(alphabets[i]);
+	}
+
+	putchar('\n');
+}
