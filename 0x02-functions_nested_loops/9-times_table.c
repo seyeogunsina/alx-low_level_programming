f372f25fc72cc9ccec99000d902017ef0a4f23b1
Adeseye <adeseyeogunsina@gmail.com>
2022-04-13 09:33:04 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+void print (int n);
+/**
+ * times_table - prints out the 9 times table
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			print(result);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
+/**
+ * print - prints an integer an
+ * @n: the integer input
+ * Return: void
+ */
+
+void print (int n)
+{
+	if (n /10)
+		print(n/10);
+	_putchar('0' + n);
+}
