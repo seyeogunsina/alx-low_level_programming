afa208d34ded8092d95fa3d9dfb68f68bb5daacd
Adeseye <adeseyeogunsina@gmail.com>
2022-04-13 14:29:50 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,11 @@ int print_last_digit(int n)
 		n *= -1;
 
 	if (n == INT_MIN)
-		n = -1 * INT_MIN;
+	{	
+		last_digit = 8;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 
 	last_digit = n % 10;
 	_putchar(last_digit + '0');
