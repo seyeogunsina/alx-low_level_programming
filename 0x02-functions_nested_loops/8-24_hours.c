38bc590e862f6ca7b589842f83956a0880883a7c
Adeseye <adeseyeogunsina@gmail.com>
2022-04-13 09:08:17 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,15 +25,7 @@ void jack_bauer(void)
 
 void print(int n)
 {
-	if (n < 10)
-	{
-		_putchar('0');
-		_putchar('0' + n);
-	}
-	else if (n > 10)
-	{
-		if (n/10)
-			print(n/10);
-		_putchar(n%10 + '0');
-	}
+	if (n/10)
+		print(n/10);
+	_putchar(n%10 + '0');
 }
