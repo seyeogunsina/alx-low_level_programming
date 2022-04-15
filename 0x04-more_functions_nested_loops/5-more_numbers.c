ce722ec82a46060df9337dda5cc5ccde13e2da9e
Adeseye <adeseyeogunsina@gmail.com>
2022-04-14 17:40:58 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+void print(int n);
+
+/**
+ * more_numbers - prints 10 lines of numbers 1 - 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			print(j);
+		_putchar('\n');
+	}
+}
+
+/**
+ * print - prints the number n
+ * @n: the imput integer
+ * Return: void
+ */
+
+void print(int n)
+{
+	if(n/10)
+		print(n/10);
+	_putchar(n % 10 + '0');
+}
