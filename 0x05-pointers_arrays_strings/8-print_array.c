801f53de314bf07040dc157749b8e44a872bfe63
Adeseye <adeseyeogunsina@gmail.com>
2022-04-21 14:03:15 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints the first n elements of an array
+ * @a: the array input
+ * @n: number of elements to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(*(a + i));
+		if (i < n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
