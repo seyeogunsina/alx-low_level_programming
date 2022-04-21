7e3e20a959f9b0d736859b7cfb64d01fbcd96f61
Adeseye <adeseyeogunsina@gmail.com>
2022-04-21 13:23:39 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other charater of a string
+ * @str: the string input
+ */
+
+void puts2(char *str)
+{
+	int l, i;
+
+	l = 0;
+	i = 0;
+
+	while (*(str + l) != '\0')
+		l++;
+	l--;
+
+	while (i <= l)
+	{
+		_putchar(*(s + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
