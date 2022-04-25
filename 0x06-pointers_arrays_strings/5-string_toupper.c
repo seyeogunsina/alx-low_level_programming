c544689fdcf05525e1360f1214701f2fdb5f9adf
Adeseye <adeseyeogunsina@gmail.com>
2022-04-24 17:07:47 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase characters to uppercase
+ * @str: the string input
+ * Return: the uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			*(str + i) = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
