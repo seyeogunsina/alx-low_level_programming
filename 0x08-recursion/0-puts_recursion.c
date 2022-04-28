78e0a483485d273685e6aaf37fd7e95daa312ec7
Adeseye <adeseyeogunsina@gmail.com>
2022-04-28 07:44:54 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the input string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
