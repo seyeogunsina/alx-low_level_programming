ea5a0f444608e38b4d3a2f361c3bec460a1ac8ef
Adeseye <adeseyeogunsina@gmail.com>
2022-04-28 10:03:39 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * prime_test - used to test for prime recursively
+ * @a: the number to be checked
+ * @b: the counter
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int prime_test(int a, int b)
+{
+	if (b == a)
+	{
+		return (1);
+	}
+	else if (b != 1 && b != a)
+	{
+		if (a % b == 0)
+			return (0);
+	}
+	else
+	{
+		prime_test(a, b + 1);
+	}
+}
+
+/**
+ * is_prime_number -  checks for prime number
+ * @n: the number input
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_test(n, 1));
+}
