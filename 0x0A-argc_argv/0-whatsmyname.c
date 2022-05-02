e5e060fb9599afb0dd179e5c4ea97710672b1521
Adeseye <adeseyeogunsina@gmail.com>
2022-05-02 15:03:39 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,13 @@
  * main - a program that prints its name
  * @argc: the number of command line arguments
  * @argv: an array of command line arguments
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int i = argc - argc;
+
 	printf("%s\n", argv[i]);
 	return (0);
 }
