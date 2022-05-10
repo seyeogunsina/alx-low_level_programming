5a0b870aea08db6f61cfd280939b8c77586e5eab
Adeseye <adeseyeogunsina@gmail.com>
2022-05-10 12:33:03 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(*ptr) * nmemb * size);
+	ptr = malloc(sizeof(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
