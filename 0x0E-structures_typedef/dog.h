07017d3ade9fdb5a60c6b5d4f2dd14cb75424b65
Adeseye <adeseyeogunsina@gmail.com>
2022-05-11 17:08:17 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a struct of a dog type
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
