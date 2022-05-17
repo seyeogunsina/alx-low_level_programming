8c428e246f5d09fe4c8364104659cb225d6702df
Adeseye <adeseyeogunsina@gmail.com>
2022-05-17 15:00:30 -0700

    standard operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[2])));
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
