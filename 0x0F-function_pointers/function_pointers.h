16ea63834831d42959a3ec5b01293c6c7dc2ca9a
Adeseye <adeseyeogunsina@gmail.com>
2022-05-17 09:48:02 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 #endif
