70c0fda9bc7e34c7aa997b992bb3ca2d83cf5083
Adeseye <adeseyeogunsina@gmail.com>
2022-05-17 14:44:04 -0700

    standard operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - used to select a function based on the operator
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		prinf("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
 
