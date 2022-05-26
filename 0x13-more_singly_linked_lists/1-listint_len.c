e429b16dd16ee8437dc6dc6c3b526b538b8e839b
Adeseye <adeseyeogunsina@gmail.com>
2022-05-26 11:16:33 -0700

    adds description

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to the linked list
+ * Return: the number of elements
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
