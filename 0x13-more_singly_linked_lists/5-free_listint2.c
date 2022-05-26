2e13cb66eb1f1d82485d27679795280f19d2a5a8
Adeseye <adeseyeogunsina@gmail.com>
2022-05-26 15:52:15 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *curr;
 
-	while ((temp = *head) != NULL)
+	if (head != NULL)
 	{
-		*head = *head->next;
-		free(temp);
-	}
+		curr = *head;
+		while ((temp = *head) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
 	*head = NULL;
 }
