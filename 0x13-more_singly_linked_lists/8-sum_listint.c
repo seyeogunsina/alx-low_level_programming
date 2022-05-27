f34efb87f5a6db3e074ee610576919eb9db0284e
Adeseye <adeseyeogunsina@gmail.com>
2022-05-26 17:05:00 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums the elements of a list
+ * @head: a pointer to the head node
+ * Return: the sum of list elements
+ */
+int sum_listint(listint_t *head)
+{
+	int tot = 0;
+
+	while (head != NULL)
+	{
+		tot += head->n;
+		head = head->next;
+	}
+	return (tot);
+}
