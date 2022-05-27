a3249965268ec76f2de0e88d08c2e2e1f69055a7
Adeseye <adeseyeogunsina@gmail.com>
2022-05-26 17:27:36 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (h == NULL && idx != 0)
+	if (ptr == NULL && idx != 0)
 		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
