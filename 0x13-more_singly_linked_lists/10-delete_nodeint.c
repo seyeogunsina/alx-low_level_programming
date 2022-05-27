cd252da4e0b756b2c284ef2f370639f64c9c4843
Adeseye <adeseyeogunsina@gmail.com>
2022-05-26 17:42:39 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		for (i = 0; i < index - 1 && prex != NULL; i++)
 		{
-			prev = prev->next
+			prev = prev->next;
 		}
 	}
 
