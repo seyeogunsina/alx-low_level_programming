b926bbba0beb166fa9161f30c4e4893a14ebeb48
Adeseye <adeseyeogunsina@gmail.com>
2022-06-02 05:43:18 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,22 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t len = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-            h = h->next;
-            len++;
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-            h = h->next;
-            len++;
-        }
-    }
-    return (len);
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			len++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			len++;
+		}
+	}
+	return (len);
 }
